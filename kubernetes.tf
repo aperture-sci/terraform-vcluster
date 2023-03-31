10670c17006459722b58c397efd0d81b7af5d3b8
Kostis Kapelonis <kostis@codefresh.io>
2023-03-31 17:38:10 +0300

    Dependency of output

--- kubernetes.tf
+++ kubernetes.tf
@@ -8,24 +8,33 @@ data "kubernetes_secret" "vcluster_kubeconfig" {
     name      = "vc-my-vcluster"
     namespace = "vcluster-demo"
   }
+
+  depends_on = [
+    helm_release.virtual_cluster
+  ]
 }
 
-output "kube_config_client_authority" {
-  value = data.kubernetes_secret.vcluster_kubeconfig.data.certificate-authority
-  sensitive = true
-}
+# output "kube_config_client_authority" {
+#   value = data.kubernetes_secret.vcluster_kubeconfig.data.certificate-authority
+#   sensitive = true
+# }
 
-output "kube_config_client_cert" {
-  value = data.kubernetes_secret.vcluster_kubeconfig.data.client-certificate
-  sensitive = true
-}
+# output "kube_config_client_cert" {
+#   value = data.kubernetes_secret.vcluster_kubeconfig.data.client-certificate
+#   sensitive = true
+# }
 
-output "kube_config_client_key" {
-  value = data.kubernetes_secret.vcluster_kubeconfig.data.client-key
-  sensitive = true
-}
+# output "kube_config_client_key" {
+#   value = data.kubernetes_secret.vcluster_kubeconfig.data.client-key
+#   sensitive = true
+# }
 
 output "kube_config" {
+  description = "The Kubeconfig file of the virtual cluster"
   value = data.kubernetes_secret.vcluster_kubeconfig.data.config
   sensitive = true
+
+
+
+
 }
