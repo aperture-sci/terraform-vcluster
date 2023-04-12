4c66cb8ac6f53228742aec8b7cf75bf482ae8ce8
Kostis Kapelonis <kostis@codefresh.io>
2023-04-12 18:14:49 +0300

    Added 3 more virtual clusters

--- deployment_clusters.tf
+++ deployment_clusters.tf
@@ -25,30 +25,51 @@ resource "helm_release" "target_vcluster_prod" {
   ]
 }
 
-# resource "helm_release" "virtual_cluster_qa" {
-#   name             = "qa-vcluster"
-#   namespace        = "vcluster-qa"
-#   create_namespace = true
-#   repository       = "https://charts.loft.sh"
-#   chart            = "vcluster"
-#   version          = "0.11.2"
-# }
+resource "helm_release" "target_vcluster_staging" {
+  name             = "target-vcluster-staging"
+  namespace        = "target-vcluster-staging"
+  create_namespace = true
+  repository       = "https://charts.loft.sh"
+  chart            = "vcluster"
+  version          = "0.11.2"
+
+  values = [
+    file("${path.module}/manifests/deployment_clusters/vcluster-staging-values.yml")
+  ]
+}
 
 
-# resource "helm_release" "virtual_cluster_staging" {
-#   name             = "staging-vcluster"
-#   namespace        = "vcluster-staging"
-#   create_namespace = true
-#   repository       = "https://charts.loft.sh"
-#   chart            = "vcluster"
-#   version          = "0.11.2"
-# }
+resource "helm_release" "target_vcluster_qa" {
+  name             = "target-vcluster-qa"
+  namespace        = "target-vcluster-qa"
+  create_namespace = true
+  repository       = "https://charts.loft.sh"
+  chart            = "vcluster"
+  version          = "0.11.2"
+
+  values = [
+    file("${path.module}/manifests/deployment_clusters/vcluster-qa-values.yml")
+  ]
+}
 
 
-resource "kubernetes_manifest" "service" {
+resource "kubernetes_manifest" "prod_ingress" {
   depends_on = [
     helm_release.target_vcluster_prod
   ]
-
   manifest = yamldecode(file("manifests/ingresses/vcluster-prod-ing.yml"))
+}
+
+resource "kubernetes_manifest" "staging_ingress" {
+  depends_on = [
+    helm_release.target_vcluster_staging
+  ]
+  manifest = yamldecode(file("manifests/ingresses/vcluster-staging-ing.yml"))
+}
+
+resource "kubernetes_manifest" "qa_ingress" {
+  depends_on = [
+    helm_release.target_vcluster_qa
+  ]
+  manifest = yamldecode(file("manifests/ingresses/vcluster-qa-ing.yml"))
 }
