7c987e4d48883e29a43cfb26d6d31c2695608c53
Kostis Kapelonis <kostis@codefresh.io>
2023-06-22 16:22:54 +0300

    Removed test clusters

--- gitop_runtime_clusters.tf
+++ gitop_runtime_clusters.tf
@@ -26,30 +26,4 @@ resource "helm_release" "virtual_cluster_staging" {
   version          = "0.11.2"
 }
 
-resource "helm_release" "eks_test" {
-  name             = "eks-vcluster"
-  namespace        = "vcluster-eks"
-  create_namespace = true
-  repository       = "https://charts.loft.sh"
-  chart            = "vcluster-eks"
-  version          = "0.11.2"
-}
 
-resource "helm_release" "k0s_test" {
-  name             = "k0s-vcluster"
-  namespace        = "vcluster-k0s"
-  create_namespace = true
-  repository       = "https://charts.loft.sh"
-  chart            = "vcluster-k0s"
-  version          = "0.11.2"
-}
-
-
-resource "helm_release" "k8s_test" {
-  name             = "k8s-vcluster"
-  namespace        = "vcluster-k8s"
-  create_namespace = true
-  repository       = "https://charts.loft.sh"
-  chart            = "vcluster-k8s"
-  version          = "0.11.2"
-}
