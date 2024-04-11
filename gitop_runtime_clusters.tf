be49da53cb0f8350005af71c7baaa984c2fe5a71
Laurent Rochette <laurent.rochette@codefresh.io>
2024-04-10 17:30:41 -0700

    Adding dev

--- gitop_runtime_clusters.tf
+++ gitop_runtime_clusters.tf
@@ -16,6 +16,14 @@
 #   version          = "0.11.2"
 # }
 
+# resource "helm_release" "virtual_cluster_dev" {
+#   name             = "dev-vcluster"
+#   namespace        = "vcluster-dev"
+#   create_namespace = true
+#   repository       = "https://charts.loft.sh"
+#   chart            = "vcluster-k0s"
+#   version          = "0.11.2"
+# }
 
 # resource "helm_release" "virtual_cluster_staging" {
 #   name             = "staging-vcluster"
